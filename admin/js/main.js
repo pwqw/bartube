cb953ddda94625b806d2e3ebbb9b09c16c9bbdb1
Alexis Caffa <AlexisCaffa@outlook.com>
2017-11-20 01:24:53 -0300

    admin con onda

--- admin/js/main.js
+++ admin/js/main.js
@@ -1,8 +1,15 @@
-var player, database;
+var $list = $('#list_youtube'),
+    $playerContainer = $('#player_container'),
+    $right = $('#right'),
+    $link = $('#link'),
+    $text = $('#text'),
+    $item,
+    player,
+    items;
 
-var tag = document.createElement('script');
+var firstScriptTag = document.getElementsByTagName('script')[0],
+    tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 
@@ -13,38 +20,21 @@ function onPlayerReady(event) {
 
 function onPlayerStateChange(event) {
     if (event.data == YT.PlayerState.ENDED) {
-        if (database.length > 1) {
-            reproducir(database[1]);
+        if (items.length > 1) {
+            reproducir(items[1]);
         }
-        firebase.database().ref('playlist/' + database[0].datetime).remove();
-        delete database[0];
+        firebase.database().ref('playlist/' + items[0].datetime).remove();
+        delete items[0];
     }
 }
 
 
-function reproducir(video) {
-    player.loadVideoById( video.id.videoId );
-}
+function reproducir(video, init) {
+    if (init) {
+        var html = videoListTemplate(video, -1, 'video_player');
+        $playerContainer.html(html);
 
-
-function makeDatabase(data) {
-    database = [];
-    for (var key in data) {
-        if(typeof data[key] !== 'function') {
-            data[key].datetime = key;
-            database.push(data[key]);
-        }
-    }
-    return database;
-}
-
-
-function onYouTubeIframeAPIReady() {
-
-    firebase.database().ref('playlist/').once('value', function (data) {
-        makeDatabase( data.val() );
-
-        var videoId = database.length > 0 ? database[0].id.videoId :'';
+        var videoId = items.length > 0 ? items[0].id.videoId :'';
         player = new YT.Player('player', {
             height: '360',
             width: '640',
@@ -54,9 +44,68 @@ function onYouTubeIframeAPIReady() {
                 'onStateChange': onPlayerStateChange
             }
         });
+    }
+    else {
+        player.loadVideoById( video.id.videoId );
+    }
+}
+
+
+function uploadVideos(data, init) {
+
+    items = [];
+    for (var key in data) {
+        if(typeof data[key] !== 'function') {
+            data[key].datetime = key;
+            items.push(data[key]);
+        }
+    }
+
+    var html = '';
+    for (var i=0; i<items.length; i++) {
+        if ( i > 0) {
+            html += videoListTemplate(items[i], i);
+        }
+        else if (init) {
+            reproducir(items[i], init);
+        }
+    }
+
+    $list.html(html);
+    $right.css({ display:'block' });
+}
+
+
+function videoListTemplate(video, i, tpl) {
+    if (video.id.kind == 'youtube#channel') {
+        return ''
+    }
+    var title = video.snippet.title,
+        published = video.snippet.publishedAt.split('T')[0].replace('-','/').replace('-','/'),
+        author = video.snippet.channelTitle,
+        thumb = video.snippet.thumbnails.high.url,
+        embed = "https://www.youtube.com/embed/" + video.id.videoId,
+        id = i,
+        html = "";
+    html = Mustache.render( Templates[tpl || 'video_list'], {
+        title : title,
+        published : published,
+        thumb : thumb,
+        author : author,
+        embed : embed,
+        id : id
+    });
+    return html;
+}
+
+
+function onYouTubeIframeAPIReady() {
+
+    firebase.database().ref('playlist/').once('value', function (data) {
+        uploadVideos( data.val(), true );
 
         firebase.database().ref('playlist/').on('value', function (data) {
-            makeDatabase( data.val() );
+            uploadVideos( data.val() );
         });
     });
 }
--- js/templates.js
+++ js/templates.js
@@ -22,11 +22,25 @@ Templates.video_list = [
     '<article class="item">',
     '<figure class="image_item"> <img src="{{ thumb }}" /> </figure>',
     '<div class="desktop">',
-    '<h2 class="title_item"><a href="#">{{ title }}</a></h2>',
-    '<p class="author_item"> Por <a href="#">{{ author }}</a></p>',
+    '<h2 class="title_item"> {{ title }} </h2>',
+    '<p class="author_item"> Por {{ author }} </p>',
     '<p class="data_item">',
     '<span class="published_item">{{ published }}</span>',
     '</p>',
     '</div>',
+    '</article>'
+].join("\n");
+
+Templates.video_player = [
+    '<article class="item">',
+    '<figure class="image_item"> <img src="{{ thumb }}" /> </figure>',
+    '<div class="desktop">',
+    '<h2 class="title_item"> {{ title }} </h2>',
+    '<p class="author_item"> Por {{ author }}</p>',
+    '<p class="data_item">',
+    '<span class="published_item">{{ published }}</span>',
+    '</p>',
+    '</div>',
+    '<div class="embed_item" id="player"></div>',
     '</article>'
 ].join("\n");
